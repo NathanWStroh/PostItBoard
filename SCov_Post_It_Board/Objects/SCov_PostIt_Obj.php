7cf40c700203b49e9c41e96b0fe6affde829aaef
nathan <nathanwstroh@gmail.com>
2016-02-08 12:53:04 -0600

    fixing file structure via git push

---
+++ SCov_Post_It_Board/Objects/SCov_PostIt_Obj.php
@@ -0,0 +1,34 @@
+<!--
+Parnter Notice Client.
+Post It Data Object 
+
+Last Modified: 2-2-2016
+--renamed class and file to better reflect the object.
+-->
+
+<?php
+class PostIts {
+	//variables
+	private $team, $partner, $issues, $issuedRep, $status; //strings
+	private $entryDate, $closeDate; //date
+	
+
+	//getters
+	function getTeam()		{ return $this->team; }
+	function getPartner()	{ return $this->partner; }
+	function getIssues()	{ return $this->issues; }
+	function getIssuedRep()	{ return $this->issuedRep; }
+	function getStatus()	{ return $this->status; }
+	function getEntryDate()	{ return $this->entryDate; }
+	function getCloseDate()	{ return $this->closeDate; }
+	
+	//setters
+	function setTeam($team) 		  {$this->team = $team; }	
+	function setPartner($partner) 	  {$this->partner = $partner; }
+	function setIssues($issues) 	  {$this->issues = $issues; }
+	function setIssuedRep($issuedRep) {$this->issuedRep = $issuedRep; }
+	function setStatus($status) 	  {$this->status = $status; }
+	function setEntryDate($entryDate) {$this->entryDate = $entryDate; }
+	function setCloseDate($closeDate) {$this->closeDate = $closeDate; }
+}
+?>
