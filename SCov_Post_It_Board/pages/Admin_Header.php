4b5a4c092b512029f7220e7f12a476fd80c55cdc
nathan <nathanwstroh@gmail.com>
2016-03-03 15:53:22 -0600

    fixed issues with links to other pages.

--- SCov_Post_It_Board/pages/Admin_Header.php
+++ SCov_Post_It_Board/pages/Admin_Header.php
@@ -3,9 +3,9 @@
     <nav class="navbar navbar-default">
         <ul class="basicNav">
             <li> <a href="../index.php">Home</a></li>
-            <li> <a href="Admin_Panel.php" >Admin Panel</a></li>
-            <li> <a href="Personal_Settings.php" >Personal Settings</a></li>            
-            <li> <a href="Post_It_Manager.php" >Post It Manager</a></li> 
+            <li> <a href="../pages/Admin_Panel.php" >Admin Panel</a></li>
+            <li> <a href="../pages/Personal_Settings.php" >Personal Settings</a></li>      
+            <li> <a href="../pages/Post_It_Manager.php" >Post It Manager</a></li> 
             <?php //will want to add in logic for log-in session ?>
             <li style="align:right;"> <a href="../pages/Admin_Panel.php" class="login" >Log in</a></li>
         </ul>
